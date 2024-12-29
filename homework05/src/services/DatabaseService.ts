e3b57e5cb4f23a782a49d50105d3d9e95be33552
Solomka Andrii <andriissolomka@gmail.com>
2024-12-29 14:16:04 +0200

    add mongoose connect service

--- homework05/src/services/DatabaseService.ts
+++ homework05/src/services/DatabaseService.ts
@@ -4,7 +4,7 @@ import logger from "../utils/loger";
 import "dotenv/config";
 import config from "config";
 
-const { log, warn, error } = logger("db_services");
+const { log, error } = logger("db_services");
 
 class DatabaseService {
   private uri: string;
@@ -16,19 +16,19 @@ class DatabaseService {
     this.dbName = dbName;
   }
 
-  async connect(): Promise<Mongoose> {
+  async connect(): Promise<any> {
     try {
-      this.client = await mongoose.connect(this.uri, { dbname: this.dbName });
+      this.client = await mongoose.connect(this.uri, { dbName: this.dbName });
       log("Connected to MongoDB");
-      return this.client.db(this.dbName);
+      return this.client;
     } catch (err) {
       error("Failed to connect to MongoDB", err);
     }
   }
 
-  async disconnect(): Promise<Db> {
+  async disconnect(): Promise<void> {
     try {
-      await this.client.close();
+      await this.client.disconnect?.();
       log("Disconnected from MongoDB");
     } catch (err) {
       error("Failed to disconnect from MongoDB", err);
@@ -40,4 +40,4 @@ const databaseUrl = config.get<string>("databaseUrl");
 const databaseName = config.get<string>("databaseName");
 
 export const databaseService = new DatabaseService(databaseUrl, databaseName);
-export const db = await databaseService.connect();
+await databaseService.connect();
