9b28cbd1e772623979a6e02869a44d5f8b43b91c
Solomka Andrii <andriissolomka@gmail.com>
2024-12-29 14:42:04 +0200

    add UserPost controller

---
+++ homework05/controllers/UserPostController.ts
@@ -0,0 +1,67 @@
+"use strict";
+import { Request, Response } from "express";
+import { ObjectId } from "mongodb";
+import { postService } from "../src/services/PostService";
+import { userPostsService } from "../src/services/UserPostsService";
+
+class UserPostController {
+  async createPost(req: Request, res: Response) {
+    try {
+      const { user_id } = req.params;
+      const { title, content, status, createdAt, updatedAt } = req.body;
+
+      const postData = {
+        authorId: new ObjectId(user_id),
+        title,
+        content,
+        status,
+        createdAt,
+        updatedAt,
+      };
+      const post = await postService.createPost(postData);
+      res.json(post);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  async getAllPosts(req: Request, res: Response) {
+    try {
+      const { user_id } = req.params;
+      const post = await userPostsService.getUserPosts(user_id);
+      res.json(post);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  async updatePost(req: Request, res: Response) {
+    try {
+      const { user_id } = req.params;
+      const { title, content, status, createdAt, updatedAt } = req.body;
+
+      const post = await userPostsService.updateUserPosts(user_id, {
+        title,
+        content,
+        status,
+        createdAt,
+        updatedAt,
+      });
+      res.json(post);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  async deletePost(req: Request, res: Response) {
+    try {
+      const { user_id } = req.params;
+      const post = await userPostsService.deleteUserPost(user_id);
+      res.json(post);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+}
+
+export default new UserPostController();
--- homework05/src/routes/userPostsRouter.ts
+++ homework05/src/routes/userPostsRouter.ts
@@ -1,68 +1,10 @@
-import { Router, Request, Response } from "express";
-import { userPostsService } from "../services/UserPostsService";
-import { postService } from "../services/PostService";
-import { ObjectId } from "mongodb";
+import { Router } from "express";
+import UserPostController from "../../controllers/UserPostController";
 
 const userPostsRouter = Router();
 
-userPostsRouter.post("/:user_id/posts", async (req: Request, res: Response) => {
-  try {
-    const { user_id } = req.params;
-    const { title, content, status, createdAt, updatedAt } = req.body;
-
-    const postData = {
-      authorId: new ObjectId(user_id),
-      title,
-      content,
-      status,
-      createdAt,
-      updatedAt,
-    };
-    const post = await postService.createPost(postData);
-    res.json(post);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-userPostsRouter.get("/:user_id/posts", async (req: Request, res: Response) => {
-  try {
-    const { user_id } = req.params;
-    const post = await userPostsService.getUserPosts(user_id);
-    res.json(post);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-userPostsRouter.put("/:user_id/posts", async (req: Request, res: Response) => {
-  try {
-    const { user_id } = req.params;
-    const { title, content, status, createdAt, updatedAt } = req.body;
-
-    const post = await userPostsService.updateUserPosts(user_id, {
-      title,
-      content,
-      status,
-      createdAt,
-      updatedAt,
-    });
-    res.json(post);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-userPostsRouter.delete(
-  "/:user_id/posts",
-  async (req: Request, res: Response) => {
-    try {
-      const { user_id } = req.params;
-      const post = await userPostsService.deleteUserPost(user_id);
-      res.json(post);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-  },
-);
+userPostsRouter.post("/:user_id/posts", UserPostController.createPost);
+userPostsRouter.get("/:user_id/posts", UserPostController.getAllPosts);
+userPostsRouter.put("/:user_id/posts", UserPostController.updatePost);
+userPostsRouter.delete("/:user_id/posts", UserPostController.deletePost);
 export default userPostsRouter;
