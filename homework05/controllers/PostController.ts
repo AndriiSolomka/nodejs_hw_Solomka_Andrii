8743d7de086d863a6d32a973ee376bc4af2482a4
Solomka Andrii <andriissolomka@gmail.com>
2024-12-29 14:36:17 +0200

    add post controller

---
+++ homework05/controllers/PostController.ts
@@ -0,0 +1,75 @@
+"use strict";
+import { Request, Response } from "express";
+import { postService } from "../src/services/PostService";
+
+class PostController {
+  async create(req: Request, res: Response) {
+    try {
+      const { authorId, title, content, status, createdAt, updatedAt } =
+        req.body;
+
+      const post = await postService.createPost({
+        authorId,
+        title,
+        content,
+        status,
+        createdAt,
+        updatedAt,
+      });
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  async getAllPosts(req: Request, res: Response) {
+    try {
+      const posts = await postService.getAllPosts(req.query);
+      res.json(posts);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  async getOnePost(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+
+      const user = await postService.getOnePost(id);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  async updatePost(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const { authorId, title, content, status, createdAt, updatedAt } =
+        req.body;
+      const user = await postService.updatePost(id, {
+        authorId,
+        title,
+        content,
+        status,
+        createdAt,
+        updatedAt,
+      });
+      res.json(user);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  async deletePost(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const message = await postService.deletePost(id);
+      res.json(message);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+}
+
+export default new PostController();
